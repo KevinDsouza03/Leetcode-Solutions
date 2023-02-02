d6d6289921bb2cb248225b03bdcd5a8c3246fffe
Kevin Dsouza <75494337+KevinDsouza03@users.noreply.github.com>
2023-02-02 16:20:59 -0500

    Time: 145 ms (43.74%), Space: 69.6 MB (15.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-            std::unordered_multimap<int, int> y;
+            unordered_set<int> y;
             for (int i = 0; i < nums.size(); i++) {
-                y.insert({nums[i],nums[i]});
-                if (y.count(nums[i]) > 1) {return true;}
+                if (y.find(nums[i]) != y.end()) return true;
+                y.insert(nums[i]);
             }
             return false;
-        /*
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                if (j == i) {
-                    if ((j+1) == nums.size()) {
-                        return false;
-                    }
-                    else {
-                        j++;
-                    }
-                }
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
-            }
-         }
-         return false;
-         */
+
     }
 };
