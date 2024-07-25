70376bf87f5c7d9208ecabdb7a5c1cafc108467b
Kevin Dsouza <75494337+KevinDsouza03@users.noreply.github.com>
2024-07-25 13:37:22 -0400

    Time: 152 ms (80.31%), Space: 28.1 MB (19.64%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,7 +4,30 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
+        left = 0
+        right = len(nums)-1
         
-        while k > 0:
-            nums.insert(0,nums.pop())
-            k -= 1
+        while left < right:
+            temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left += 1
+            right -= 1
+        
+        left = 0
+        right = k-1
+        while left < right:
+            temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left += 1
+            right -= 1
+        
+        left = k
+        right = len(nums)-1
+        while left < right:
+            temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left += 1
+            right -= 1
