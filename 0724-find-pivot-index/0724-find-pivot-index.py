3aca7deaadac1832bdbc93a12b65f7c2ee791349
Kevin Dsouza <75494337+KevinDsouza03@users.noreply.github.com>
2024-07-27 12:52:33 -0400

    Time: 160 ms (17.25%), Space: 19.2 MB (6.78%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,15 +1,26 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         
-        totalSum = sum(nums)
-
+        leftEdges = {}
+        rightEdges = {}
         
-        leftSum = 0
-        for index,cur in enumerate(nums):
-            if leftSum == totalSum-cur-leftSum:
-                return index
-            else:
-                leftSum+= cur
         
-    
+        temp = 0
+        for index,num in enumerate(nums):
+            leftEdges.update({index:temp})
+            temp+= num
+        
+        temp = 0
+        for i in range(len(nums)-1,-1,-1):
+            rightEdges.update({i:temp})
+            temp+= nums[i]
+        
+        print(leftEdges)
+        print(rightEdges)
+        
+        for i in leftEdges.keys():
+            if leftEdges[i] == rightEdges[i]:
+                return i
+        
+        
         return -1
