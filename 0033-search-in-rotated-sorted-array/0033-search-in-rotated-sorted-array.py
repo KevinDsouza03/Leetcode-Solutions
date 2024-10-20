91fadc13a0b532f06f6f55275921df90aa10de75
Kevin Dsouza <75494337+KevinDsouza03@users.noreply.github.com>
2024-10-19 20:32:47 -0400

    Time: 0 ms (100%), Space: 17 MB (10.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,28 +1,23 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        l,r = 0, len(nums)-1
-        
+        #binary searc
+
+        l,r = 0, len(nums) - 1
+
         while l <= r:
-            mid = (l+r)//2
+            mid = (l + r) // 2
+
             if target == nums[mid]:
                 return mid
-            #now check which portion of arr
-            
-            #left
-            if nums[mid] >= nums[l]:
+
+            if nums[l] <= nums[mid]:
                 if target > nums[mid] or target < nums[l]:
                     l = mid + 1
                 else:
-                    r = mid-1
-                
-                
-            else: #right
+                    r = mid - 1
+            else:
                 if target < nums[mid] or target > nums[r]:
-                    r = mid -1
+                    r = mid - 1
                 else:
                     l = mid + 1
-                    
-                    
         return -1
-        
