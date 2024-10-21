ab8aa659a7946100c9bd460f741852a5437ad801
Kevin Dsouza <75494337+KevinDsouza03@users.noreply.github.com>
2024-10-21 11:21:38 -0400

    Time: 0 ms (100%), Space: 17.1 MB (70.76%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,30 +1,32 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        srow = 0
-        scol = 0
-        erow = len(matrix)-1
-        ecol = len(matrix[0])-1
-        print(f'erow{erow},ecol{ecol}')
-        while matrix[srow][scol] < matrix[erow][ecol]:
-            if matrix[srow][scol] == target:
-                return True
-            elif matrix[erow][ecol] == target:
-                return True
-            if (matrix[srow][scol] < target):
-                if (scol+1 > len(matrix[0])-1):
-                    srow += 1
-                    scol = 0
-                else:
-                    scol += 1
-            if (matrix[erow][ecol] > target):
-                if (ecol-1 < 0):
-                    erow -= 1
-                    ecol = len(matrix[0])-1
-                else:
-                    ecol -= 1
-        if matrix[srow][scol] == target:
-            return True
-        elif matrix[erow][ecol] == target:
-            return True
-        return False
+        #so to search for taret, we can binary search the first of every row.
+        #walkthrough:
+        #1 < 3, go next 10 > 3, ok 1 is our row. Binary search again on inner row
+        # next:
+        # 1 < 13, go next, 10 < 13, go next, 23 < 
+
+        #finding row
+
+        row = 0
+
         
+        for i in range(0,len(matrix)):
+            if matrix[i][0] == target or matrix[i][len(matrix[i])-1] == target:
+                return True
+            if matrix[i][0] < target and matrix[i][len(matrix[i])-1] > target:
+                row = i
+                break
+        
+        l, r = 0, len(matrix[row])-1
+
+        while l <= r:
+            mid = l + (r-l) // 2
+            if matrix[row][mid] == target:
+                return True
+            elif matrix[row][mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
+            
